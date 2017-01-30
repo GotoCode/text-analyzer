d0a5e79e92dbd514bfa42da89011e0c24868b265
GotoCode <khalid.peer233@gmail.com>
2017-01-30 16:09:13 +0300

    Implemented code for counting vowels

---
+++ main.py
@@ -0,0 +1,71 @@
+# 
+# main logic for text analyzer program
+# 
+# NOTE: This project requires Python 3 to work properly
+# 
+
+
+# FEATURE LIST #
+
+# count vowels
+# count consonants
+# count words
+# "calculate" mood from text
+# report summary of analysis
+# read in text from command line
+# read in text from file
+# read in text from url
+
+
+# imports #
+
+
+# constants #
+
+VOWELS = "aeiou"
+
+# helper functions #
+
+def num_vowels(s):
+    '''
+    Given a string, returns a dict mapping each vowel
+    to a count of number of occurrences
+    '''
+    
+    counts = {}
+
+    for ch in s:
+        
+        if ch.isalpha() and ch in VOWELS:
+            counts[ch] = counts.get(ch, 0) + 1
+    
+    return counts
+
+def num_consonants(s):
+    '''
+    Given a string, returns a dict mapping each consonant
+    to a count of number of occurrences
+    '''
+    
+    pass
+
+def num_words(s):
+    '''
+    Given a string, returns a dict mapping each word
+    to a count of number of occurrences
+    '''
+    
+    pass
+
+
+# main logic #
+
+def main():
+    
+    pass
+
+
+# boilerplate code #
+
+if __name__ == '__main__':
+    main()
