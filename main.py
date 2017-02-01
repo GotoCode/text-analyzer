024ace7ac771b949b97672ec4237102d0538c84f
GotoCode <khalid.peer233@gmail.com>
2017-02-01 13:30:26 +0300

    Add graphing capability via matplotlib

--- main.py
+++ main.py
@@ -8,8 +8,6 @@
 # FEATURE LIST #
 
 # improve code for argument parsing to use "argparse" module
-# report longest and shortest word
-# support for graphing frequencies via matplotlib
 # add support for adaptive (non hard-coded) display of output number 'widths'
 # remove extraneous formatting from input text (i.e. ! . ; ? etc.)
 # implement stemmer/lemmatizer to improve semantic analysis algorithm
@@ -20,6 +18,7 @@
 # fix 'a nexus of pure evil' bug
 # add ability to read from multiple files and compile results into one summary
 # add ability to export analysis results to JSON
+# add '--graph-words' or '--graph-chars' or '--graph' flag option
 
 
 # imports #
@@ -27,6 +26,7 @@
 from __future__ import print_function
 
 import requests
+import matplotlib.pyplot as plt
 
 import sys
 import os
@@ -280,6 +280,61 @@ def __print_summary_from_url(url):
     else:
         
         __print_summary_from_string(r.content)
+
+def __graph_chars(info):
+    '''
+    Given a tuple of summary stats
+    such as that returned by report_summary
+    this function plots character frequency 
+    data via matplotlib
+    '''
+
+    # retrieve info from summary tuple
+    v_counts = info[0]
+    c_counts = info[1]
+
+    v_counts.update(c_counts)
+
+    # set up data for x- and y-axes of data plot
+    x_labels = [x for x, _ in sorted(v_counts.items())]
+
+    x = list(range(len(v_counts)))
+    y = [y for _, y in sorted(v_counts.items())]
+
+    # plot bar graph of letter vs frequency
+
+    bar_width = 1/1.5
+
+    plt.bar(x, y, width=bar_width, tick_label=x_labels)
+    
+    plt.show()
+
+def __graph_words(info):
+    '''
+    This function is similar to the __graph_chars
+    function, except that it plots word frequencies
+    instead of character frequencies
+    '''
+
+    # retrieve appropriate summary stats
+    w_counts = info[2]
+
+    # set up data for x- and y-axes
+    x = list(range(len(w_counts)))
+    y = [c for _, c in sorted(w_counts.items())]
+
+    x_labels = [w for w, _ in sorted(w_counts.items())]
+
+    # plot frequencies on a bar graph
+    plt.xticks(x, x_labels, rotation='vertical')
+
+    bar_width = 1/1.5
+
+    plt.bar(x, y, width=bar_width)
+
+    plt.subplots_adjust(bottom=0.15)
+
+    plt.show()
 
 # main logic #
 
