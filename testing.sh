4cf5ec77555541e98e724d388b42f57df965beef
GotoCode <khalid.peer233@gmail.com>
2017-02-15 11:25:57 +0300

    Added shell script to automate testing

---
+++ testing.sh
@@ -0,0 +1,3 @@
+python main.py -s "the cat in the hat" "who knew he could fly" "when the candy is in the house, then you may eat it"
+python main.py -f test*.txt
+python main.py -u 'https://en.wikipedia.org/wiki/Main_Page' 'https://news.ycombinator.com'
